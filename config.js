8672cfae0ac64a9cfb9d0b7e5c36da546db55919
Silent042 <144302036+Silent042@users.noreply.github.com>
2024-08-12 11:59:48 +0100

    Update config.js
    Signed-off-by: Silent042 <144302036+Silent042@users.noreply.github.com>

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const DATABASE_URL =
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.SESSION_ID || '2348168855791').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
@@ -29,11 +29,11 @@ module.exports = {
           logging: false,
         }),
   HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
-  SUDO: process.env.SUDO || '',
+  SUDO: process.env.SUDO || '2348168855791',
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
   HEROKU_API_KEY: process.env.HEROKU_API_KEY,
   BRANCH: 'master',
-  STICKER_PACKNAME: process.env.STICKER_PACKNAME || 'GiftedTech',
+  STICKER_PACKNAME: process.env.STICKER_PACKNAME || 'SILENT',
   ALWAYS_ONLINE: toBool(process.env.ALWAYS_ONLINE),
   LOG_MSG: toBool(process.env.LOG_MSG) || false,
   RMBG_KEY: process.env.RMBG_KEY || 'DJwc34NaWmhXmDxEeh6YUEmm',
